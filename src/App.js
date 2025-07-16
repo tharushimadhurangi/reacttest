dc128a9cb957eb2ea3e3b08eda4e19a705b1e808
tharushimadhurangi <kdtharushim@gmail.com>
2025-07-16 15:22:36 +0530

    this is front end

---
+++ src/App.js
@@ -0,0 +1,19 @@
+ import './App.css';
+import Main from './Components/Main';
+
+function App() {
+  return (
+ <>
+  <div>
+    Tharushi Madhurangi
+  <h1>Amila</h1>
+  <p> Upul</p>
+  <Main>
+     <h5> Tharushi Madhurangi</h5>
+  </Main>
+  </div>
+  </>
+  )
+}
+
+export default App;
---
+++ src/Components/Main.js
@@ -0,0 +1,18 @@
+function Main(props){
+const name ="Amila";
+const position ="web development";
+    return (
+    <div>
+        This is main components
+        <p>tharushi Madhurangi</p>
+        <p>sfdfgghhghtj</p>
+        <ul>
+            <li>{name}</li>
+            <li>{position}</li>
+        </ul>
+        
+        
+        </div>
+        );
+}
+export default Main;
